5f841b2bb017844d86d91de81daf4ce64d808e02
Vinitpal <vs361017@gmail.com>
2021-11-02 12:13:06 +0530

    updated placements section

--- src/components/Home-page/Placement/index.jsx
+++ src/components/Home-page/Placement/index.jsx
@@ -1,13 +1,34 @@
+import byjus from "/Images/byjus.png";
+import facebook from "/Images/facebook.png";
+import google from "/Images/google.png";
+import jio from "/Images/jio.png";
+import tcs from "/Images/tcs.png";
+import vedantu from "/Images/vedantu.png";
+import paytm from "/Images/paytm.png";
+import wipro from "/Images/wipro.png";
+
 const index = () => {
+  const imgArr = [
+    { title: byjus },
+    { title: facebook },
+    { title: google },
+    { title: jio },
+    { title: tcs },
+    { title: vedantu },
+    { title: paytm },
+    { title: wipro },
+  ];
+
   return (
     <div className="h-screen flex justify-evenly items-center bg-gray-100">
-      <div className="w-5/12 flex flex-col items-center">
+      <div className="w-6/12 flex flex-col items-center">
         <h1 className="text-5xl font-semibold my-10">Our Top Recruiters</h1>
         <div className="flex flex-wrap justify-center text-center mb-4 ">
-          {[...Array(10)].map((_, i) => (
+          {imgArr.map((img, i) => (
             <img
-              className="m-4 w-24"
-              src="https://cdn.discordapp.com/attachments/753151975570276352/904658365001646080/368px-Google_2015_logo.png"
+              key={i}
+              className="my-4 mx-10 w-28"
+              src={img.title}
               alt="recruiter"
             />
           ))}
